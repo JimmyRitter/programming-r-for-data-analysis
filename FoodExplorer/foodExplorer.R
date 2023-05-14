80cb81a28d34711cb473e4e156dd6d94f9594034
Jimmy Ritter <jimmy.ritter@redacreltd.com>
2023-05-14 20:50:41 +0200

    creating R project, R markdown, and resolving exercise 1 using R

---
+++ FoodExplorer/foodExplorer.R
@@ -0,0 +1,9 @@
+food_data <- read.csv("data/foodData.csv")
+
+#Challenge:
+# 1. Which category of food has the highest average price?
+# 2. What is the most common country of origin for the food items?
+# 3. Calculate the average calories per serving for the food items in the "Salad" category.
+# 4. How many food items have a protein content greater than 20 grams?
+# 5. Determine the distribution of carbohydrate content for the food items.
+# 6. What is the average fat content of the food items in the "Pasta" category?
